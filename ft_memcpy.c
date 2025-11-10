7bd63e68af5d100aab57f5df4d205b7dddc437f9
Adnane Ouassari <Adnaneouassari@gmail.com>
2025-11-10 18:19:27 +0100

    adding a NULL check

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -17,7 +17,11 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char	*d;
 	unsigned char	*s;
-
+	
+	if (!dest && !src)
+    	return (NULL);
+	if (n == 0 || dest == src)
+    	return (dest);
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
 	while (n > 0)
